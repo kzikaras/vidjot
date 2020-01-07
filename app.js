04be337f0ffa60164b85fc56504b7620e19d7931
Kurt Zikaras <kurtzikaras@gmail.com>
2020-01-06 20:37:32 -0500

    fixed port

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ app.use('/users', users);
 // Use ideas routes
 app.use('/ideas', ideas);
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
